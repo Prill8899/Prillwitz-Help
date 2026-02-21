4e91ad0b0694607f1bc4c7e63546b577461ae3f3
Alfredo <TU_EMAIL>
2026-02-21 16:47:58 -0300

    restore catch-all redirect

---
+++ pages/[[...slug]].js
@@ -0,0 +1,9 @@
+﻿export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "https://www.chatbase.co/help/AC8_WXwytVVhOpedaxdEH",
+      permanent: false,
+    },
+  };
+}
+export default function CatchAll(){ return null; }
