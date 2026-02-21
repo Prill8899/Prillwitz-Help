3eec0d9dd8288b4e8fada3486840b987e3b18362
Alfredo <TU_EMAIL>
2026-02-21 16:14:02 -0300

    move rewrites to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import("next").NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/__cb/:path*",
+        destination: "https://www.chatbase.co/__cb/:path*",
+      },
+      {
+        source: "/api/chat/AC8_WXwytVVhOpedaxdEH/:path*",
+        destination: "https://www.chatbase.co/api/chat/AC8_WXwytVVhOpedaxdEH/:path*",
+      },
+      {
+        source: "/:path*",
+        destination: "https://www.chatbase.co/help/AC8_WXwytVVhOpedaxdEH",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
